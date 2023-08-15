bf96f9e2dc9463f6053c6d1ace1f700065592f2c
unknown <gaze17hanif@gmail.com>
2023-08-15 23:25:05 +0700

    Ini buat dashboard user

--- daftar_mapel.php
+++ daftar_mapel.php
@@ -26,35 +26,40 @@
   <!-- header -->
   <div class="container-fluid py-3 d-flex align-items-center bg_color1">
     <div class="container-xxl">
-      <div class="row w-100">
-        <div class="col-lg-4 col-sm-12 col-xs-6">
-          <div class="row h-100 justify-content-center">
-            <img class="img-fluid" src="image 6.png" alt="">
+      <div class="row bg-warning">
+        <div class="col-lg-4 col-md-6 col-sm-8 col-8 bg-info">
+          <div class="row h-100 justify-content-center align-content-center">
+            <img class="img-fluid img_navbar_mobile" src="image 6.png" alt="">
           </div>
         </div>
-        <div class="col-lg-2 offset-lg-6 col-5 offset-8 col-sm-4 offset-sm-9 d-flex align-items-center">
-          <div class="row h-100 w-100 align-items-center justify-content-center bg_color4">
-            <div class="col-lg-2 offset-lg-2 col-2 size_33 d-flex justify-content-center align-items-center bg-info">
-              <img src="user.png" alt="" class="size_33 bg-danger">
-            </div>
-            <div class="col-lg-7 col-8 height_33px ps-2 d-flex align-items-center justify-content-center bg-danger" style="width: 107px;">
-              <!-- <?php echo "untuk nama dari user" ?> -->
-              hhhj
 
-              <!-- Ini nanti ada pakai javascript -->
-              <!-- <a href="">untuk logut</a> -->
-              <!-- <a href="">untuk ke halaman profile</a> -->
+        <div class="d-flex col-lg-2 offset-lg-6 col-md-6 col-sm-4 col-4 p-0 justify-content-end align-items-center bg-success">
+          <!-- Nama Profil -->
+          <div class="container_profil_daftar_mapel bg-warning" id="btn_logout">
+            <div class="d-flex align-items-center profil_daftar_mapel w-100 bg-light">
+              <div class="img_profil_daftar_mapel bg-danger">
+                <img src="user.png" alt="">
+              </div>
+              <div class="d-flex justify-content-center align-items-center text_profil_daftar_mapel w-100 bg-warning" >
+                Munandar
+              </div>
             </div>
           </div>
+          <!-- Nama Profil -->
 
-          <!-- <div class="row w-100 row-cols-lg-2 ps-lg-1 row-cols-sm-2 row-cols-2 gx-0 bg_color3">
-                    <div class="col ms-lg-5 size_33 d-flex align-items-center bg-warning">
-                      <img class="img-fluid size_33" src="user.png" alt="">
-                    </div>
-                    <div class="col height_33px d-flex align-items-center justify-content-center bg-danger" style="width: 96px;">
-                      
-                    </div>
-                  </div> -->
+          <!-- Tombol detail profil dan logout -->
+          <div class="container_profil_daftar_mapel bg-warning d-none" id="btn_show">
+            <div class="profil_daftar_mapel_mobile bg-white">
+              <div class="d-flex justify-content-center align-items-center profil_user bg-info">
+                <a href="">Profil</a>
+              </div>
+              <div class="d-flex justify-content-center align-items-center logout_user bg-danger">
+                <a href="">Logout</a>
+              </div>
+            </div>
+          </div>
+          <!-- Tombol detail profil dan logout -->
+
         </div>
       </div>
     </div>
@@ -62,72 +67,70 @@
   <!-- header -->
 
   <!-- content -->
-  <div class="container-fluid">
+  <div class="container-fluid bg_color3" id="back">
     <div class="container-xxl">
       <div class="container">
-        <div class="mt-2">
-          <a href=""></a><img class="bg_color1" src="dasboard-logo.png" alt=""></a>
+        <div class="pt-2">
+          <a href="daftar_mapel.php"><img class="img_dashboard_user bg_color1" src="dasboard-logo.png" alt=""></a>
         </div>
-        <div class="row mt-2 mb-4">
-          <div class="col-12 bg_color3">
-            <div class="row py-2">
-              <div class="col-1" style="width: 75px;">
-                <img class="" src="book.png" alt="">
+        <div class="row mt-2 mb-lg-4 mb-md-4 mb-3">
+          <div class="col-12 bg_color4">
+            <div class="d-flex align-items-center py-2">
+              <div class="d-flex align-items-center container_img_book_user">
+                <img class="img_book_user" src="book.png" alt="">
               </div>
-              <div class="col-11 gx-0 d-flex align-items-center">
-                <h3>Daftar Mata Pelajaran</h3>
+              <div class="d-flex align-items-center text_mapel_user w-100">
+                Daftar Mata Pelajaran
               </div>
             </div>
           </div>
         </div>
 
-        <div class="row row-cols-1 row-cols-md-3 g-4 mb-4">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 pb-4">
           <!-- LAKUKAN PERULANGAN -->
           <!-- <?php //for() ?> -->
-            <div class="col">
-              <div class="card h-100">
-                <div class="bg-warning ">
-                  <img src="mapel-sub.png" class="card-img-top img-fluid" alt="...">
-                </div>
-                <div class="card-body">
-                  <!-- <h5 class="card-title"><?php ?></h5> -->
-                  <div class="row">
-                    <div class="col-6">
-                      <!-- <p class="card-title fs-5"><?php ?></p> -->
-                      <p class="card-title fs-5">IPA</p>
-                    </div>
-                    <div class="col-6 text-end">
-                      <img src="there_dot.png" alt="">
-                    </div>
+          <div class="col">
+            <div class="card card_daftar_mapel">
+              <div class="d-flex justify-content-center align-items-center card-img-top bg-warning">
+                <img src="mapel-sub.png" class="card-img-top img_card_daftar_mapel" alt="...">
+              </div>
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-6">
+                    <!-- <p class="card-title fs-5"><?php ?></p> -->
+                    <p class="card_title">IPA</p>
                   </div>
-                  <!-- <h2 class="card-text"><?php ?></h2> -->
-                  <h2 class="card-text">FISIKA</h2>
+                  <div class="col-6 text-end">
+                    <img src="there_dot.png" alt="">
+                  </div>
                 </div>
+                <!-- <h2 class="card-text"><?php ?></h2> -->
+                <h2 class="card_text">FISIKA</h2>
               </div>
             </div>
+          </div>
 
-            <!-- CONTOH JIKA ADA 2 MAPEL -->
-            <div class="col">
-              <div class="card h-100">
-                <div class="bg-warning ">
-                  <img src="mapel-sub.png" class="card-img-top img-fluid" alt="...">
-                </div>
-                <div class="card-body">
-                  <!-- <h5 class="card-title"><?php ?></h5> -->
-                  <div class="row">
-                    <div class="col-6">
-                      <!-- <p class="card-title fs-5"><?php ?></p> -->
-                      <p class="card-title fs-5">IPA</p>
-                    </div>
-                    <div class="col-6 text-end">
-                      <img src="there_dot.png" alt="">
-                    </div>
+          <!-- CONTOH JIKA ADA 2 MAPEL -->
+          <div class="col">
+            <div class="card card_daftar_mapel">
+              <div class="d-flex justify-content-center align-items-center card-img-top bg-warning">
+                <img src="mapel-sub.png" class="card-img-top img_card_daftar_mapel" alt="...">
+              </div>
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-6">
+                    <!-- <p class="card-title fs-5"><?php ?></p> -->
+                    <p class="card_title">IPA</p>
                   </div>
-                  <!-- <h2 class="card-text"><?php ?></h2> -->
-                  <h2 class="card-text">FISIKA</h2>
+                  <div class="col-6 text-end">
+                    <img src="there_dot.png" alt="">
+                  </div>
                 </div>
+                <!-- <h2 class="card-text"><?php ?></h2> -->
+                <h2 class="card_text">FISIKA</h2>
               </div>
             </div>
+          </div>
           <!-- <?php ?> -->
         </div>
       </div>
@@ -138,7 +141,7 @@
   <!-- footer -->
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12 py-3 bg_color3">
+      <div class="col-12 py-3 bg_color1">
         <div class="container text_footer">
           <p>Copyright © 2023 by Elearning SMAN 1 KASIHAN</p>
         </div>
@@ -146,6 +149,25 @@
     </div>
   </div>
   <!-- footer -->
+
+  <script>
+    // The classList property returns the CSS classnames of an element.
+    const btn = document.getElementById("btn_logout");
+    const btnShow = document.getElementById("btn_show").classList;
+    const back = document.getElementById("back");
+    const btnClass = btn.classList;
+    
+
+    btn.addEventListener('click',  () => {
+      btnClass.add('d-none')
+      btnShow.remove('d-none')
+    })
+
+    back.addEventListener('click', () => {
+      btnClass.remove('d-none')
+      btnShow.add('d-none')
+    })
+  </script>
 </body>
 
 </html>
